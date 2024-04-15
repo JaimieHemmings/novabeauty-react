5c68294ee549c50d7d37dfb838cfecc78ebb17dd
Jaimie <jaimie.j.hemmings@gmail.com>
2024-04-15 19:11:30 +0100

    Update index.jsx

--- src/components/_partials/Panels/index.jsx
+++ src/components/_partials/Panels/index.jsx
@@ -14,35 +14,35 @@ const Panels = () => {
         <section className="services">
             <div className="container">
                 <div className="row">
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Nails} alt="" width="400" height="400" />
                         <h2>Nails</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Massage} alt="" width="400" height="400" />
                         <h2>Massage</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Microdermabrasion} alt="" width="400" height="400" />
                         <h2>Microdermabrasion</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Waxing} alt="" width="400" height="400" />
                         <h2>Waxing</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Facials} alt="" width="400" height="400" />
                         <h2>Facials</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Lashes} alt="" width="400" height="400" />
                         <h2>Lashes</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Hair} alt="" width="400" height="400" />
                         <h2>Hair</h2>
                     </Link>
-                    <Link to="/">
+                    <Link to="/services">
                         <img src={Brows} alt="" width="400" height="400" />
                         <h2>Brows</h2>
                     </Link>
