d3e83d3014f7fedc0d4c18ac931601f5d06f2e3a
Jaimie <jaimie.j.hemmings@gmail.com>
2024-04-17 12:08:15 +0100

    Additional content to about page and Navbar changes

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,15 +1,34 @@
 import Hero from '../_partials/Hero'
 import Testimonials from '../_partials/Testimonials'
+import FaceImage from '../../assets/images/face-line.png'
+import Swiper from "../_partials/Swiper"
 import './index.css'
 
 const About = () => {
     return (
         <>
+            <Swiper />
             <Hero
                 title={"About Us"}
                 p1={"If you need some more information or would like to book an appointment with me, please use the form below!"}
                 p2={"I can't wait to hear from you!"}
             />
+            <section className="about-us" style={{ backgroundImage: `url(${FaceImage})`}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h2>VISUAL INTEGRITY,<br />
+                            SIMPLE FORMS AND<br />
+                            STRIVING FOR<br />
+                            HARMONY</h2>
+                        </div>
+                        <div className="col">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, dolores. Quisquam quasi id alias odit, dicta dolore accusamus similique ducimus, veritatis vero optio earum quam repellat eum adipisci amet provident!</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, dolores. Quisquam quasi id alias odit, dicta dolore accusamus similique ducimus, veritatis vero optio earum quam repellat eum adipisci amet provident!</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <Testimonials />
         </>
     )
--- src/components/_partials/Navigation/index.jsx
+++ src/components/_partials/Navigation/index.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import './index.css'
 
 const Navigation = () => {
@@ -10,29 +14,57 @@ const Navigation = () => {
     };
 
     return (
-        <nav>
-            <div className="container navigation">
-                <div className="row">
-                    <div className="col">
-                        <a href="/" className="logo">NOVA</a>
-                    </div>
-                    <div className="col">
-
-                        <a href="#"
-                            className="menuToggle"
-                            id="menuBtn"
-                            onClick={ToggleClass}
-                        >Menu</a>
-
-                        <ul className={isActive ? "menu" : "menu active"} id="menu">
-                            <li><NavLink to="/about" onClick={ToggleClass}>About</NavLink></li>
-                            <li><NavLink to="/services" onClick={ToggleClass}>Services</NavLink></li>
-                            <li><NavLink to="/contact" onClick={ToggleClass}>Contact</NavLink></li>
-                        </ul>
+        <>
+            <section className="contact-bar">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <ul>
+                                <li>
+                                    <a href="mailto:rhiannah1993@gmail.com">
+                                        <i><FontAwesomeIcon icon={faEnvelope} /></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/profile.php?id=61551603390209" target="_blank">
+                                        <i><FontAwesomeIcon icon={faFacebookSquare} /></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/nova.beautynails/" target="_blank">
+                                        <i><FontAwesomeIcon icon={faInstagram} /></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </section>
+            <nav>
+                <div className="container navigation">
+                    <div className="row">
+                        <div className="col">
+                            <a href="/" className="logo">NOVA</a>
+                        </div>
+                        <div className="col">
+
+                            <a href="#"
+                                className="menuToggle"
+                                id="menuBtn"
+                                onClick={ToggleClass}
+                            >Menu</a>
+
+                            <ul className={isActive ? "menu" : "menu active"} id="menu">
+                                <li><NavLink to="/" onClick={ToggleClass}>Home</NavLink></li>
+                                <li><NavLink to="/about" onClick={ToggleClass}>About</NavLink></li>
+                                <li><NavLink to="/services" onClick={ToggleClass}>Services</NavLink></li>
+                                <li><NavLink to="/contact" onClick={ToggleClass}>Contact</NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
