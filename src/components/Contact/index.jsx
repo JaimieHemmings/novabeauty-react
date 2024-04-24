ffe3673d19b5eb85c9e0f564d1e371188e0a1802
Jaimie <jaimie.j.hemmings@gmail.com>
2024-04-24 16:22:10 +0100

    Small Contact Page update

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,19 @@
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import Hero from '../_partials/Hero'
 import Testimonials from '../_partials/Testimonials'
+import 'leaflet/dist/leaflet.css'
 import './index.css'
 
 const Contact = () => {
     return (
         <>
+        
+            <MapContainer center={[50.4362682, -4.7904315]} zoom={9} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
             <Hero
                 title={"Contact Us"}
                 p1={"If you need some more information or would like to book an appointment with me, please use the form below!"}
